0b3a654919c942aa800343623fb299f89cb1367d
kanchanpagad <kanchanpagad111@.com>
2023-05-26 18:29:12 +0530

    js  practice

---
+++ HTMLprograms/js/firstway.js
@@ -0,0 +1,19 @@
+// What are Variables?
+// Variables are containers for storing data (storing data values).
+// 4 Ways to Declare a JavaScript Variable:
+// Using var
+// Using let
+// Using const
+// Using nothing
+
+
+//syntax let/var/const/nothing keyword variable_name = value/data;
+let test1; //decration of variable e.g test1
+test1 = 'kanchan123'; //definations
+console.log(test1); //verifcation of value
+
+let test = "kanchan123"; //declaration + defination
+console.log(test);
+
+let firstName ="kanchan";
+console.log(firstName);
---
+++ HTMLprograms/js/thirdway.js
@@ -0,0 +1,20 @@
+function test(){
+    test = 'kanchan';
+    xyz = 2020;
+   document.write(test);
+   console.log(test);
+}
+
+let firstName = 'kanchan';
+console.log(firstName);
+
+function testParametrisedFun(a,b){
+ let res = a + b;
+ console.log(res);
+}
+
+function parameterLessFun(){
+   let result = 20 + 90 ;
+   console.log(result);
+}
+
