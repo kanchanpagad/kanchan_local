5021f9fde80b5b42ac4eba73bf7083513dc95c31
KanchanLocal_1 <kanchanpagad111@gmail.com>
2023-07-22 01:58:37 -0600

    add

--- HotelManagement/src/app/app.module.ts
+++ HotelManagement/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    LandingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
